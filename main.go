a1c9fc8d37d3b09ff12526e27e73ba057d398438
Morgan Sundqvist <morgan.sundqvist@gmail.com>
2024-06-29 18:18:35 +0200

    fix: forgot to init config

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/morgansundqvist/memosgpt/config"
 	"github.com/morgansundqvist/memosgpt/handlers"
 )
 
 func main() {
+	config.InitConfig()
+
 	app := fiber.New()
 
 	println("Server started on port 3214")
